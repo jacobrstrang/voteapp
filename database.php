84cfa7689e55b4298fe1c538bdceb2a4f38f3b19
Jacob Strang <jakerstrang@gmail.com>
2013-12-11 01:31:07 -0600

    MySQL instructions in database.php

--- database.php
+++ database.php
@@ -5,4 +5,13 @@ if($mysqli->connect_errno) {
     printf("Connection Failed: %s\n", $mysqli->connect_error);
     exit;
 }
+
+/*
+    MySQL databases are as follows:
+        choices(id TINYINT(3) UNSIGNED NOT NULL AUTO_INCREMENT, name VARCHAR(200) NOT NULL)
+        
+        votes(id MEDIUMINT(8) UNSIGNED NOT NULL AUTO_INCREMENT, number VARCHAR(15) NOT NULL, vote TINYINT(3) UNSIGNED)
+        
+        The id field is the primary key for both, and votes.vote is a foreign key to choices.id
+ */
 ?>
